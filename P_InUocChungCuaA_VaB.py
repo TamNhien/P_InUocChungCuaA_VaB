1471b7f05bd16bc5aa96630f1bb432a79e0738a0
TamNhien <canhthien91@gmail.com>
2024-03-22 21:29:58 +0700

    Add project files.

---
+++ P_InUocChungCuaA_VaB.py
@@ -0,0 +1,6 @@
+﻿a = int(input("Nhập vào số nguyên dương a : "))
+b = int(input("Nhập vào số nguyên dương b : "))
+
+gcd = max(i for i in range(1, min(a, b) + 1) if a % i == 0 and b % i == 0)
+print("Ước chung lớn nhất của", a, "và", b, "là : ", gcd)
+
